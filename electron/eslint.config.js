37cf72b5cfe88451d338f08f4cfe7543ba7df8a8
huzefa9246 <huzefa9246@gmail.com>
2026-05-03 15:58:07 +1000

    added lint for electron and fixed electron

---
+++ electron/eslint.config.js
@@ -0,0 +1,20 @@
+const js = require('@eslint/js')
+const globals = require('globals')
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+]
